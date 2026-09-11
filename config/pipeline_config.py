70412d72bb0e91c41afc7104ae53d90a1906a3d5
Ferrdd <3051735@gmail.com>
2026-09-11 13:13:18 +0300

    fix

--- config/pipeline_config.py
+++ config/pipeline_config.py
@@ -5,8 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Data_Pipeline_Settings(BaseSettings):
-    raw_data_path: str
-    converted_data_path: str
+    raw_data_path: Path = Path("data/raw")
+    converted_data_path: Path = Path("data/converted")
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
