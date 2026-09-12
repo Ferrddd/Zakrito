6ccdaeeec9856b5745fdfc94629bb996c391c930
Ferrdd <123326656+Ferrddd@users.noreply.github.com>
2026-09-12 14:39:58 +0300

    simpe data loader

--- src/data_pipeline/loader.py
+++ src/data_pipeline/loader.py
@@ -1,3 +1,23 @@
-from config import pipeline_config
+import pandas as pd
 
-settings = pipeline_config.data_pipeline_settings
+from src.data_pipeline import pipeline_models
+
+settings = pipeline_models.data_pipeline_settings
+
+class DataPreparer:
+    def __init__(self, settings):
+        self.file_avt = settings.file_avt
+        self.file_242000 = settings.file_242000
+
+    def load_data(self):
+        df_avt = pd.read_csv(self.file_avt, engine='pyarrow')
+        df_242000 = pd.read_csv(self.file_242000, engine='pyarrow')
+
+        df = pd.merge(df_avt, df_242000, on='date', how='inner')
+        return df
+
+    def clean_data(self, df):
+        df.drop(columns=['Unnamed: 0.1', 'Unnamed: 0'])
+        df["date"] = df.to_datetime(df["date"])
+
+        return df
